f938aec32c87e8aa522e590f6143ac036440f7a2
Victor Flucus <victorflucus@gmail.com>
2022-03-18 07:54:29 -0400

    updating projects

--- Dev/small-projects/exercises/settings.py
+++ Dev/small-projects/exercises/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    'clickToAddItems',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'exercises.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_portfolio',
+        'USER': 'root',
+        'PASSWORD': '9Trumpet$',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
